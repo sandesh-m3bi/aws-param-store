ea2a579f2661f7c9085151302a8d31711092075c
Sandesh Gaonkar <gaonkas@M3bi-India.com>
2019-02-12 12:10:32 +0530

    created new project for aws parameter store

---
+++ src/main/java/com/aws/paramstore/awsparamstore/AwsParamStoreApplication.java
@@ -0,0 +1,14 @@
+package com.aws.paramstore.awsparamstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsParamStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsParamStoreApplication.class, args);
+	}
+
+}
+
